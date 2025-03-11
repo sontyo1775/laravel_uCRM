8bda81175b3a6012e75ebb6437f9927e6b661096
PASOKON\kingu <awuyuuichisai@gmail.com>
2025-03-12 00:39:51 +0900

    テストビュー１

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+// add 20250312 No18
+import { Link } from '@inertiajs/vue3';
+// add 20250312 No18
+
+</script>
+<template>
+<h1>InertiaTestです</h1>
+<a href="/">aタグ経由</a><br>
+<Link href="/">Link経由</Link>
+</template>
