d3eb177c982a20d4a40d08e3c9ca1575801a86a1
PASOKON\kingu <awuyuuichisai@gmail.com>
2025-03-19 02:57:10 +0900

    コメント追加

--- database/migrations/2025_03_19_022321_create_customers_table.php
+++ database/migrations/2025_03_19_022321_create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             // $table->id();
             // $table->timestamps();
+            // 20250319 add No51 ダミーデーター1000件投入
             $table->id();
             $table->string('name');
             $table->string('kana');
