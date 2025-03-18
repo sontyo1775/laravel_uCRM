d49dd2288cbd2bcc8e7ccb61a262159837ed2771
PASOKON\kingu <awuyuuichisai@gmail.com>
2025-03-19 02:50:14 +0900

    ダミーデーター1000件投入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+// add 20250319 ダミーデータ1000件挿入
+use App\Models\Customer;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        // add 20250319 ダミーデータ1000件挿入
+        Customer::factory(1000)->create();
 
         // add 20250316 ItemSeeder No41
         $this->call([
